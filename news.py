cd5ab1e452b5a05083e1fb7a46977e9ac32b8165
SechelTov <83236030+SechelTov@users.noreply.github.com>
2022-11-24 12:39:05 +0200

    Update news.py

--- news.py
+++ news.py
@@ -1,3 +1,33 @@
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime as dt
+import time
+from pandas import option_context
+
+def news(OLD_NEWS):
+    p = 'https://www.inn.co.il/flashes'
+    r = requests.get(p)
+    soup = BeautifulSoup(r.content)
+    soup = soup.find_all('div', {'class':"accordeon-item__header"} )
+    
+    for div in soup:
+        if div.text not in OLD_NEWS:
+            OLD_NEWS = [div.text] + OLD_NEWS # i want the most new to be first
+            #print (div.text)
+    return OLD_NEWS
 
 frequency = st.slider('How often do you want the news to be updated (in seconds):', 1, 60)
+
+LAST_NEWS = []
+LAST_NEWS_TEST = news(LAST_NEWS)
+if LAST_NEWS != LAST_NEWS_TEST:
+    LAST_NEWS = LAST_NEWS_TEST
+    df = pd.DataFrame([ LAST_NEWS[i].split("\n")[0:2] for i in range(0,len(LAST_NEWS_TEST))],columns=['time','news'])
+    reversed_df = df.iloc[::-1]
+
+    st.dataframe( reversed_df.head(15), width=400)
+
+time.sleep(frequency)
+
