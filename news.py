319605345f768da81cb1aeb8b5af9cd160c3366a
SechelTov <83236030+SechelTov@users.noreply.github.com>
2022-11-24 12:48:33 +0200

    Update news.py

--- news.py
+++ news.py
@@ -18,17 +18,17 @@ def news(OLD_NEWS):
             #print (div.text)
     return OLD_NEWS
 
-frequency = st.slider('How often do you want the news to be updated (in seconds):', 1, 60)
+frequency = st.slider('How often do you want the news to be updated (in seconds):', 1, 60, 5)
 
 LAST_NEWS = []
-while True:
-    LAST_NEWS_TEST = news(LAST_NEWS)
-    if LAST_NEWS != LAST_NEWS_TEST:
-        LAST_NEWS = LAST_NEWS_TEST
-        df = pd.DataFrame([ LAST_NEWS[i].split("\n")[0:2] for i in range(0,len(LAST_NEWS_TEST))],columns=['time','news'])
-        reversed_df = df.iloc[::-1]
 
-        st.dataframe( reversed_df.head(15) )
+LAST_NEWS_TEST = news(LAST_NEWS)
+if LAST_NEWS != LAST_NEWS_TEST:
+    LAST_NEWS = LAST_NEWS_TEST
+    df = pd.DataFrame([ LAST_NEWS[i].split("\n")[0:2] for i in range(0,len(LAST_NEWS_TEST))],columns=['time','news'])
+    reversed_df = df.iloc[::-1]
 
-    time.sleep(frequency)
+    st.dataframe( reversed_df.head(15) )
+
+time.sleep(frequency)
 
